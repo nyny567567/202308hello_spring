5b23ecd291f5308fc7abbfb188cc91ebca9f80e8
nyny567567 <ny567@naver.com>
2023-08-15 19:16:52 +0900

    멤버 서비스 테스트

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
@@ -38,7 +42,7 @@ public class MemberService {
 
     }
 
-    public Optional<Member> findOnd(Long memberId){
+    public Optional<Member> findOne(Long memberId){
         return memberRepository.findById(memberId);
 
     }
